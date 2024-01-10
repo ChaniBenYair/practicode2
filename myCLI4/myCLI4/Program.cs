f826a2b3e1914783889fdcc703c17a36e0f18ab8
Chani Ben-Yair <149004273+ChaniBenYair@users.noreply.github.com>
2024-01-11 01:11:56 +0200

    first commit

---
+++ myCLI4/myCLI4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.CommandLine;
+
+var bundleCommand = new Command("bundle", "nnn");
+bundleCommand.SetHandler(() =>
+{
+    Console.WriteLine("bundle command");
+});
+
+var rootCommand = new RootCommand("root command");
+rootCommand.AddCommand(bundleCommand);
+rootCommand.InvokeAsync(args);
